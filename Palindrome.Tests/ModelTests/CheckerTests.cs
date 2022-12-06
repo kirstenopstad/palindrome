8035ca82736cb9e9aedc4cbff292a405bf3f66f5
Kirsten Opstad <kirsten.opstad@gmail.com>
2022-12-06 13:44:15 -0800

    passes all tests and meets MVP
    Co-authored-by: Brian Noh <noh24@ymail.com>

--- Palindrome.Tests/ModelTests/CheckerTests.cs
+++ Palindrome.Tests/ModelTests/CheckerTests.cs
@@ -1,17 +1,56 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using ProjectName;
+using Palindrome.Models;
 
-namespace ProjectName.Tests
+namespace Palindrome.Tests
 {
   [TestClass]
-  public class ClassNameTests
+  public class CheckerTests
   {
     [TestMethod]
     // Test methods will go here
-    public void NameOfMethodWeAreTesting_DescriptionOfBehavior_ExpectedReturnValue()
+    public void IsPalindrome_StringIsPalindrome_True()
     {
-      // any necessary logic to prep for test; instantiating new classes, etc.
-      // Template: ``` Assert.AreEqual(EXPECTED_RESULT, CODE_TO_TEST); ```
+      string userInput = "ana";
+      Checker checker = new Checker();
+      Assert.AreEqual(true, checker.IsPalindrome(userInput));
+    }
+
+    [TestMethod]
+    public void IsPalindrome_StringIsNotPalindrome_False()
+    {
+      string userInput = "nana";
+      Checker checker = new Checker();
+      Assert.AreEqual(false, checker.IsPalindrome(userInput));
+    }
+    [TestMethod]
+    public void IsPalindrome_CaseSensitiveStringIsPalindrome_True()
+    {
+      string userInput = "aNana"; // anaNa
+      Checker checker = new Checker();
+      Assert.AreEqual(true, checker.IsPalindrome(userInput));
+    }
+    [TestMethod]
+    public void IsPalindrome_CaseSensitiveStringIsNotPalindrome_False()
+    {
+      string userInput = "nAna"; // anAn
+      Checker checker = new Checker();
+      Assert.AreEqual(false, checker.IsPalindrome(userInput));
+    }
+    
+    [TestMethod]
+    public void IsPalindrome_NumStringIsPalindrome_False()
+    {
+      string userInput = "322"; // 223
+      Checker checker = new Checker();
+      Assert.AreEqual(false, checker.IsPalindrome(userInput));
+    }
+
+    [TestMethod]
+    public void IsPalindrome_NumStringIsNotPalindrome_True()
+    {
+      string userInput = "323"; // 323
+      Checker checker = new Checker();
+      Assert.AreEqual(true, checker.IsPalindrome(userInput));
     }
   }
 }
