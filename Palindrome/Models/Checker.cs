25bddab133ba9d79aff0c9ff1ff97d020246f916
Kirsten Opstad <kirsten.opstad@gmail.com>
2022-12-06 09:32:04 -0800

    add initial project info & structure
    Co-authored-by: Brian Noh <noh24@ymail.com>

---
+++ Palindrome/Models/Checker.cs
@@ -0,0 +1,22 @@
+namespace Palindrome
+{
+  public class Checker
+  {
+    // Get user input (string)
+    // Store string as array
+
+    // Iterate through array 
+      // make all lowercase 
+      // create a reverse copy array
+
+    // Iterate through length and check reverse against original array
+      // If all are ==, return true
+      // Else return false 
+
+    // NOTES
+    // "Kirsten" false || "Brian" false || "Abba" true
+    // "123" false || "456" false || "323" true
+    // "0abba0" true
+    // properties, constructors, methods, etc. go here
+  }
+}
